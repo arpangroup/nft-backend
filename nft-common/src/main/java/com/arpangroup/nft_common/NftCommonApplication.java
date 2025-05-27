12d0ae2cc38c6b91bbf6851a6fe5e46ae4575e22
arpan jana <arpangroup1@gmail.com>
2025-05-28 02:05:29 +0530

    refactor commission

---
+++ nft-common/src/main/java/com/arpangroup/nft_common/NftCommonApplication.java
@@ -0,0 +1,13 @@
+package com.arpangroup.nft_common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NftCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NftCommonApplication.class, args);
+	}
+
+}
---
+++ nft-common/src/test/java/com/arpangroup/nft_common/NftCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arpangroup.nft_common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NftCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
