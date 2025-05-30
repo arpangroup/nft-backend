4c855b91243a3ca1ca84fc95661fb6247f1387f7
arpan jana <arpangroup1@gmail.com>
2025-05-31 01:34:23 +0530

    refactor the code

--- nft-core/src/main/java/com/arpangroup/nft_core/controller/HelloController.java
+++ nft-core/src/main/java/com/arpangroup/nft_core/controller/HelloController.java
@@ -6,40 +6,17 @@ import com.arpangroup.user_service.entity.User;
 import com.arpangroup.user_service.service.RegistrationService;
 import com.arpangroup.user_service.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/api/v1")
+@Controller
 @RequiredArgsConstructor
 public class HelloController {
-    private final UserService userService;
-    private final RegistrationService registrationService;
-    private final ReferralPingService referralPingService;
 
-//    public HelloController(@Qualifier("userServiceImpl") UserService userService, ) {
-//        this.userService = userService;
-//    }
-
-
-    @GetMapping("/hello")
-    public List<User> sayHello() {
-        return userService.getUsers();
-    }
-
-    @GetMapping("/ping")
-    public String ping() {
-        return referralPingService.ping();
-    }
-
-    @GetMapping("/register")
-    public User registerDemoUser() {
-        RegistrationRequest request = new RegistrationRequest();
-        request.setUsername("apple");
-        request.setReferralCode("REF1");
-        return registrationService.registerUser(request);
+    @GetMapping
+    public String index() {
+        return "index";
     }
 }
